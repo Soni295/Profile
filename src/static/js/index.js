323f44a6d8703eba2e76508afe191f0dc235c040
Sion Ramos de los Santos <sionandrieux@hotmail.com>
2021-05-02 13:30:26 -0300

    add mor styles and sort repositores

--- src/static/js/index.js
+++ src/static/js/index.js
@@ -1,1 +1,19 @@
-console.log('Hello World')
+const folder = document.getElementById('folder')
+import { projects } from './projects.js'
+
+const templateProject = ({endPoint, name, img='example.png'}) => `
+<div class='project'
+  style='background-image: url("/public/img/${img}");'
+>
+  <a
+    href="https://github.com/Soni295/${endPoint}"
+    target="_blank"
+    rel="noreferrer noopener"
+  >${name}
+  </a>
+</div>
+`
+
+folder.innerHTML = projects.reduce(
+  (acc, cur) => acc + templateProject(cur) , ''
+)
---
+++ src/static/js/projects.js
@@ -0,0 +1,11 @@
+export const projects = [
+  {"name": "ClickArt", "endPoint": "ClickArt"},
+  {"name": "Snake Game", "endPoint": "Snake-Game"},
+  {"name": "InstaClon", "endPoint": "InstaClon"},
+  {"name": "Replace px for rem", "endPoint": "ReplacePxForRem"},
+//  {"name": "Timer", "endPoint": "TimerJs"},
+  {"name": "Chat Server", "endPoint": "Chat-ts"},
+  {"name": "Dog team", "endPoint": "dog-team"},
+  {"name": "Princing Toggle", "endPoint": "Princing-Toggle"},
+  {"name": "Easybank landing page", "endPoint": "Easybank-landing-page"}
+]
